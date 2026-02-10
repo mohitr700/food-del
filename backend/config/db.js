ec717f8ab38d5f22d897a29065e34c9b028f769a
Kumar Mohit Raj <mohitrajnwd1@gmail.com>
2026-02-11 01:17:49 +0530

    mongodb issue fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect(
-      'mongodb+srv://mohit:060379@cluster0.ct8gj8k.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose
+      .connect(process.env.MONGO_URL)
+      .then(() => console.log("DB Connected"));
 }
